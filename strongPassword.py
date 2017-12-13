5e8668cc80f1eec21cbf24d11d99a7e22b612a0c
Jason Pauley <jasonpauley@gmail.com>
2017-12-12 20:04:13 -0800

    Update strongPassword.py

--- strongPassword.py
+++ strongPassword.py
@@ -12,12 +12,12 @@
 import pyperclip, re
 
 passwordRegex = re.compile(r'''(
-    ^(?=.*[A-Z].*[A-Z])                # at least two capital letters
+    (?=.*[A-Z].*[A-Z])                # at least two capital letters
     (?=.*[!@#$&*])                     # at least one of these special characters
     (?=.*[0-9].*[0-9])                 # at least two numeric digits
-    (?=.*[a-z].*[a-z].*[a-z])          # at least three lower case letters
+    (?=.*[a-z]{3,})                  # at least three lower case letters
     .{10,}                              # at least 10 total digits
-    $
+    
     )''', re.VERBOSE)
 
 def userInputPasswordCheck():
